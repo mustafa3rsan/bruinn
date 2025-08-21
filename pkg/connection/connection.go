6592086fe257b03c1ccf8b2a71ec25835fa5e0c4
mustafa3rsan <134810075+mustafa3rsan@users.noreply.github.com>
2025-08-21 15:31:25 +0300

    Add fluxx support to connection manager

--- pkg/connection/connection.go
+++ pkg/connection/connection.go
@@ -2101,3 +2101,38 @@ func NewManagerFromConfig(cm *config.Config) (config.ConnectionAndDetailsGetter,
 	wg.Wait()
 	return connectionManager, errList
 }
+
+
+
+// Add import:
+"github.com/bruin-data/bruin/pkg/fluxx"
+
+// Add to Manager struct:
+Fluxx    map[string]*fluxx.Client
+
+// Add connection method:
+func (m *Manager) AddFluxxConnectionFromConfig(connection *config.FluxxConnection) error {
+	m.mutex.Lock()
+	if m.Fluxx == nil {
+		m.Fluxx = make(map[string]*fluxx.Client)
+	}
+	m.mutex.Unlock()
+
+	client, err := fluxx.NewClient(&fluxx.Config{
+		Instance: connection.Instance,
+		ClientId: connection.ClientId,
+		ClientSecret: connection.ClientSecret,
+	})
+	if err != nil {
+		return err
+	}
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+	m.Fluxx[connection.Name] = client
+	m.availableConnections[connection.Name] = client
+	m.AllConnectionDetails[connection.Name] = connection
+	return nil
+}
+
+// Add to NewManagerFromConfig:
+processConnections(cm.SelectedEnvironment.Connections.Fluxx, connectionManager.AddFluxxConnectionFromConfig, &wg, &errList, &mu)
