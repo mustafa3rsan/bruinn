33dea582d3dd1c51cba21b7d871a5b0c5afbe8b3
mustafa3rsan <134810075+mustafa3rsan@users.noreply.github.com>
2025-08-21 15:31:28 +0300

    Add test cases for fluxx connection

--- pkg/config/manager_test.go
+++ pkg/config/manager_test.go
@@ -1996,3 +1996,27 @@ environments:
 		})
 	}
 }
+
+
+
+// Add test case to TestConfig_AddConnection tests array:
+{
+	name:     "Add Fluxx connection",
+	envName:  "default",
+	connType: "fluxx",
+	connName: "fluxx-conn",
+	creds: map[string]interface{}{
+		"instance": "test-instance",
+		"client_id": "test-client-id",
+		"client_secret": "test-client-secret",
+	},
+	expectedErr: false,
+},
+
+// Add test assertion case:
+case "fluxx":
+	assert.Len(t, env.Connections.Fluxx, 1)
+	assert.Equal(t, tt.connName, env.Connections.Fluxx[0].Name)
+	assert.Equal(t, tt.creds["instance"], env.Connections.Fluxx[0].Instance)
+	assert.Equal(t, tt.creds["client_id"], env.Connections.Fluxx[0].ClientId)
+	assert.Equal(t, tt.creds["client_secret"], env.Connections.Fluxx[0].ClientSecret)
